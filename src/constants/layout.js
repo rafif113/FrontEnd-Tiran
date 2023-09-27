ac1ed568dd0cca65767c50f5a96886052c023723
Rafif Yusuf Avandy <49221528+rafif113@users.noreply.github.com>
2023-09-27 22:54:12 +0700

    first init

---
+++ src/constants/layout.js
@@ -0,0 +1,11 @@
+const INIT_STATE = {
+  layoutType: layoutTypes.HORIZONTAL,
+  leftSidebarType: leftSidebarTypes.DARK,
+  layoutModeType: layoutModeTypes.LIGHTMODE,
+  layoutWidthType: layoutWidthTypes.FLUID,
+  layoutPositionType: layoutPositionTypes.FIXED,
+  topbarThemeType: topbarThemeTypes.LIGHT,
+  leftsidbarSizeType: leftsidbarSizeTypes.DEFAULT,
+  leftSidebarViewType: leftSidebarViewTypes.DEFAULT,
+  leftSidebarImageType: leftSidebarImageTypes.NONE,
+};
